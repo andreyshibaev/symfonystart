0eec97aea1338a300a23bfea8f6a66181010d8fc
Андрей Шибаев <andreishibaev@yandex.ru>
2023-12-10 10:08:23 +0700

    update composer

--- rector.php
+++ rector.php
@@ -2,28 +2,24 @@
 
 declare(strict_types=1);
 
-//use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
-
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__ . '/config',
+//        __DIR__ . '/node_modules',
         __DIR__ . '/public',
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ]);
 
-    // register a single rule
-//    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-    $rectorConfig->rule(TypedPropertyFromStrictConstructorRector::class);
-    // define sets of rules
-    //    $rectorConfig->sets([
-    //        LevelSetList::UP_TO_PHP_81
-    //    ]);
+
     $rectorConfig->sets([
-      SetList::CODE_QUALITY
+        SetList::CODE_QUALITY,
+        LevelSetList::UP_TO_PHP_82,
+        SetList::DEAD_CODE,
+        SetList::CODING_STYLE
     ]);
 };
